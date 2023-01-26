eb35f8dee05488a24cbcf157ebbaffa83edb1f7d
TQMINHVN <tq-minh@b-create.co.jp>
2023-01-26 22:40:37 +0700

    fx

--- server.js
+++ server.js
@@ -13,18 +13,18 @@ app.use(express.json());
 app.use(express.static('public'))
 // dbConnect();
 //chan cors error
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+//   );
+//   next();
+// });
 
 app.get("/", function (req, res) {
   res.send("home !!!");
