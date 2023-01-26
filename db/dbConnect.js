09393182ae7d8b25a184850c992a27cce9cffb66
TQMINHVN <tq-minh@b-create.co.jp>
2023-01-26 22:55:34 +0700

    fix

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -1,19 +1,20 @@
 require("dotenv").config();
 
 async function dbConnect() {
-    const mongoose = require("mongoose");
-    mongoose.set("strictQuery", false);
-    const mongoString = process.env.MONGODB_URI;
-    mongoose.connect(mongoString);
-    const database = mongoose.connection;
-    
-    database.on("error", (error) => {
-      console.log(error);
-    });
-    
-    database.once("connected", () => {
-      console.log("Database Connected");
-    });
+  const mongoose = require("mongoose");
+  mongoose.set("strictQuery", false);
+  const mongoString =
+    "mongodb+srv://admin:!123456@cluster-mongo-crud.eojyp7u.mongodb.net/testDatabase";
+  mongoose.connect(mongoString);
+  const database = mongoose.connection;
+
+  database.on("error", (error) => {
+    console.log(error);
+  });
+
+  database.once("connected", () => {
+    console.log("Database Connected");
+  });
 }
 
 module.exports = dbConnect;
