026317009cab45dcb1e393ba32c04d942a316c9f
TQMINHVN <tq-minh@b-create.co.jp>
2023-01-27 00:12:03 +0700

    fx

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const Users = require("../modals/users");
 const users = express.Router();
 const jwt = require("jsonwebtoken");
